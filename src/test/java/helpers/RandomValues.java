dad23c76863deeb3723317236b7d2491daf780ee
javaAutoTest <marine.melk.qa@mail.ru>
2022-08-01 14:56:07 +0400

    connected db

--- src/test/java/helpers/RandomValues.java
+++ src/test/java/helpers/RandomValues.java
@@ -1,32 +1,57 @@
 package helpers;
 
-import java.util.Random;
-
 public class RandomValues {
-    public String GetRandomString(int n) {
-        // lower limit for LowerCase Letters
-        int lowerLimit = 97;
+//    public String GetRandomString(int n) {
+//        // lower limit for LowerCase Letters
+//        int lowerLimit = 97;
+//
+//        // lower limit for LowerCase Letters
+//        int upperLimit = 122;
+//
+//        Random random = new Random();
+//
+//        // Create a StringBuffer to store the result
+//        StringBuffer r = new StringBuffer(n);
+//
+//        for (int i = 0; i < n; i++) {
+//
+//            // take a random value between 97 and 122
+//            int nextRandomChar = lowerLimit
+//                    + (int) (random.nextFloat()
+//                    * (upperLimit - lowerLimit + 1));
+//
+//            // append a character at the end of bs
+//            r.append((char) nextRandomChar);
+//        }
+//
+//        // return the resultant string
+//        return r.toString();
+//    }
+//}
 
-        // lower limit for LowerCase Letters
-        int upperLimit = 122;
 
-        Random random = new Random();
-
-        // Create a StringBuffer to store the result
-        StringBuffer r = new StringBuffer(n);
-
-        for (int i = 0; i < n; i++) {
-
-            // take a random value between 97 and 122
-            int nextRandomChar = lowerLimit
-                    + (int) (random.nextFloat()
-                    * (upperLimit - lowerLimit + 1));
-
-            // append a character at the end of bs
-            r.append((char) nextRandomChar);
+    public long currentDateTime() {
+            long currentDateTime = System.currentTimeMillis();
+            return currentDateTime;
         }
 
-        // return the resultant string
-        return r.toString();
+        public String randomName(){
+            String createRandomName = "user" + currentDateTime();
+            return  createRandomName;
+        }
+    public String randomEmail(){
+        String createRandomEmail = "user" + currentDateTime() + '@' + "testing.com";
+        return  createRandomEmail;
     }
-}
+
+    public String randomGender(){
+        String createRandomGender = currentDateTime()%2==0 ? "male":"female";
+        return  createRandomGender;
+    }
+
+    public String randomStatus(){
+        String createRandomStatus = currentDateTime()%2==0 ? "active":"inactive";
+        return  createRandomStatus;
+    }
+    }
+
--- src/test/java/tests/gorestTests.java
+++ src/test/java/tests/gorestTests.java
@@ -1,11 +1,15 @@
 package tests;
 
 import api.Specifications;
+import helpers.ConnectToDB;
+import helpers.RandomValues;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +20,16 @@ import static org.hamcrest.Matchers.notNullValue;
 public class gorestTests {
     private final static String URL = "https://gorest.co.in";
     private final static String token = "a9e5f1e7c2b7ddc3308af58fa9c251e2fb38809ee71d26e37b780419acae502e";
+    protected String userId = "";
+    RandomValues randomValues = new RandomValues();
+    ConnectToDB db = new ConnectToDB();
+    protected String randName = randomValues.randomName();
+    protected String randGender = randomValues.randomGender();
+    protected String randEmail = randomValues.randomEmail();
+    protected String randStatus = randomValues.randomStatus();
+
+    public gorestTests() throws SQLException {
+    }
 
     @Test
     void checkAllUsers(){
@@ -37,15 +51,33 @@ public class gorestTests {
         List<String> emails = jsonPath.get("data.email");
         Assertions.assertTrue(emails.stream().allMatch(x->x.contains("@")));
     }
+    @Test
+    void getOneUser(){
+        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpec200());
+        Response response = given()
+                .urlEncodingEnabled(false)
+                .basePath("/public-api/users")
+                //.pathParam("userId", encode("{userId}", UTF_8.name()))
+                .when()
+                .get("/{userId}")
+                .then().log().all()
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        int status = jsonPath.get("code");
+        List<String> names = jsonPath.get("data.name");
+        Assertions.assertEquals(200,status);
+        Assertions.assertTrue(names.stream().anyMatch(x->x.equals(randName)));
+    }
+
 
     @Test
     void createNewUser(){
         Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpec(200));
         Map<String, String> user = new HashMap<>();
-        user.put("name","New User17");
-        user.put("gender","male");
-        user.put("email","user17@mail.ru");
-        user.put("status","active");
+        user.put("name",randName);
+        user.put("gender",randGender);
+        user.put("email",randEmail);
+        user.put("status",randStatus);
         Response response = given()
                 .body(user)
                 .auth().oauth2(token)
@@ -55,6 +87,8 @@ public class gorestTests {
                 .extract().response();
         JsonPath jsonPath = response.jsonPath();
         int status = jsonPath.get("code");
+        userId = jsonPath.get("data.id").toString();
+        System.out.println(userId);
         System.out.println(status);
         Assertions.assertEquals(201,status);
     }
@@ -63,15 +97,15 @@ public class gorestTests {
     void createExistingNewUser(){
         Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpec(200));
         Map<String, String> user = new HashMap<>();
-        user.put("name","New User17");
-        user.put("gender","male");
-        user.put("email","user17@mail.ru");
-        user.put("status","active");
+        user.put("name",randName);
+        user.put("gender",randGender);
+        user.put("email",randEmail);
+        user.put("status",randStatus);
         Response response = given()
                 .body(user)
                 .auth().oauth2(token)
                 .when()
-                .post("/public-api/users")
+                .post("/public-api/users/")
                 .then().log().all()
                 .extract().response();
         JsonPath jsonPath = response.jsonPath();
@@ -84,22 +118,25 @@ public class gorestTests {
 
     @Test
     void updateUser(){
-        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpec(201));
+        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpec(200));
         Map<String, String> user = new HashMap<>();
-        user.replace("name","Updated User");
-        user.replace("email","updatedUser@mail.ru");
-        user.replace("status","inactive");
+        user.replace("name",randomValues.randomName());
+        user.replace("email",randomValues.randomEmail());
+        user.replace("status",randStatus);
+        //System.out.println(userId);
         Response response = given()
                 .body(user)
                 .auth().oauth2(token)
                 .when()
-                .put("/public-api/users")
+                .patch("/public-api/users/:{userId}")
                 .then().log().all()
                 .extract().response();
         JsonPath jsonPath = response.jsonPath();
-        String name = jsonPath.get("name");
+        int status = jsonPath.get("code");
+        String id = jsonPath.get("data.id").toString();
         System.out.println(response.getStatusCode());
-        Assertions.assertEquals("New User", name);
+        Assertions.assertEquals(200,status);
+        Assertions.assertEquals(id,userId);
     }
 
     @Test
@@ -121,5 +158,10 @@ public class gorestTests {
         String name = jsonPath.get("name");
         System.out.println(response.getStatusCode());
         Assertions.assertEquals("Updated User", name);
+    }
+
+    @Test
+    void DB() throws SQLException, IOException {
+        db.connectToDB();
     }
 }
